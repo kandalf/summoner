5d400d0c2c531fc51b28f5ff97a0849a63bc67bc
Leonardo Mateo <leonardomateo@gmail.com>
2011-01-15 19:00:13 -0300

    Refactor specs for isolated scenarios

--- spec/vampires_spec.rb
+++ spec/vampires_spec.rb
@@ -5,22 +5,27 @@ class Vampire < Monster
   attr_accessor :name
 end
 
-Summoner.prepare :vampire do |m|
-  m.name "Dracula"
-end
 
 describe Vampire do
-  it "should be Vlad" do
+  before :all do
+    Summoner.prepare :vampire do |m|
+      m.name "Dracula"
+    end
+  end
+
+  it "should override attributes only once" do
+    edward = Summoner.summon :vampire, :name => "Edward"
+    edward.name.should == "Edward"
     vlad = Summoner.summon :vampire
     vlad.name.should == "Dracula"
   end
 
-  it "should be Edward" do
-    edward = Summoner.summon :vampire, :name => "Edward"
+  it "should override attributes only once when a block is given" do
+    edward = Summoner.summon :vampire do |v|
+      v.name "Edward"
+    end
     edward.name.should == "Edward"
-  end
-
-  it "should be Vlad again" do
-    Summoner.summon(:vampire).name.should == "Dracula"
+    vlad = Summoner.summon :vampire
+    vlad.name.should == "Dracula"
   end
 end
